b77524f906a1a466cc7f84c7f853fcf9e414d916
pomahtuk <pman89@ya.ru>
2016-12-01 22:32:44 +0100

    CI immitation for single run of client tests, some dummy tests

---
+++ client/src/components/Todo/Todo.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Todo from './Todo';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Todo
+      onClick={() => true}
+      completed={false}
+      text="todo"
+    />,
+    div
+  );
+});
--- client/src/containers/App/App.test.js
+++ client/src/containers/App/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './App';
+import reducer from '../../reducers';
+
+const store = createStore(reducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={store} />, div);
 });
